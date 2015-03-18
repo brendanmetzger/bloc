2ee753e73ae309c1dc8a220f8ca17abe8c75bead
Brendan <brendan.metzger@gmail.com>
2015-03-18 17:03:55 -0500

    working on a way to slug/iterate data

--- view/plat.php
+++ view/plat.php
@@ -14,12 +14,43 @@ namespace bloc\view;
 class Plat
 {
   
-  public function __construct($node)
+  public function __construct($view, $data)
   {
-    $this->node = $node;
+    foreach ($view->parser->queryCommentNodes('iterate') as $key => $node) {
+      $matched = $this->map($view, $node->nextSibling);
+      
+      $nodelist = substr($matched->item(0)->nodeValue, 2,-1);
+        
+      foreach ($data->{$key} as $datum) {
+        \bloc\console::dump($datum);
+      }
+    }
   }
-
   
+  public function map($view, $context)
+  {
+    # start with the current element and look for nodes
+    $exp = "./descendant-or-self::*[";
+    # if starts with the open placeholder
+    $exp .= "substring(.,1,1) = '[' and ";
+    # and contains the variable symbole key
+    $exp .= "contains(., '@') and ";
+    # and ends with the close placeholder
+    $exp .= "substring(., string-length(.), 1) = ']' and ";
+    # and does not contain any other nodes
+    $exp .= "not(*)]";
+    # union search for the attribute nodes
+    $exp .= "|./descendant-or-self::*/@*[";
+    # if it starts with the open placeholder
+    $exp .= "substring(.,1,1) = '[' and ";
+    # and it contains the variable symbol key
+    $exp .= "contains(., '@') and ";
+    # and ends with the close placeholder
+    $exp .= "substring(., string-length(.), 1) = ']']";
+    
+    \bloc\console::dump($exp);
+    return $view->xpath->query($exp, $context);
+  }
   
   public function __clone()
   {
