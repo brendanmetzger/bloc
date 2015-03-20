3e2be0162a827f97fc6a351161912d9276858d55
Brendan <brendan.metzger@gmail.com>
2015-03-20 18:36:11 -0500

    refactored. < 75 lines. :)

--- view/plat.php
+++ view/plat.php
@@ -4,66 +4,41 @@ namespace bloc\view;
 
 /**
  * Plat
-
-   This workhorse is an interesting specimen. Feed it a view in the constructor. Should you 
-  be interested in mapping data to the node (and you should), pass in a chunk of data that has 
-  keys (object or array) as well.
-
+ * This workhorse is an interesting specimen. Feed it a view in the constructor. Should you 
+ * be interested in mapping data to the node (and you should), pass in a chunk of data (object or array) that has 
+ * matching keys in [the @novel template format] as well.
  */
 
 class Plat
-{
-  
+{  
   public function __construct($view, $data)
   {
-    
+
     // cycle through iterators and see to their needs first
     foreach ($view->parser->queryCommentNodes('iterate') as $key => $node) {
+
       $context = $node->parentNode->removeChild($node->nextSibling);
       $matched = $this->getSlugs($view, $context);
 
       if (property_exists($data, $key)) {
-
+        
         foreach ($data->{$key} as $datum) {
           ksort($datum);
           
           foreach ($matched as $template) {
-            /*
-              TODO seriously consider adding this to getSlugs method. would need to execute another loop.. would be nice to have a benchmark.
-            */
-            if (!property_exists($template, 'slug')) {
-              preg_match_all('/\@([a-z\_\:0-9]+)\b/i', substr($template->nodeValue, 1, -1), $matches);
-              $template->matches = array_combine($matches[1], $matches[0]);
-              ksort($template->matches);
-              $template->slug = substr($template->nodeValue, 1,-1);
-            }
-            
             $template->nodeValue = str_replace($template->matches, array_intersect_key($datum, $template->matches), $template->slug);
-          
           }
-          
           $node->parentNode->insertBefore($context->cloneNode(true), $node);
         }
       }
-      
-      
-      $node->parentNode->removeChild($node);
-      
+      $node->parentNode->removeChild($node);      
     }
     
     // find document wide placeholders
-    $matched = $this->getSlugs($view, $view->dom->documentElement);
-    foreach ($matched as $node) {
-      $slug = substr($node->nodeValue, 1,-1);
-      // 2 is attribute, 1 is element
-      $node->nodeValue = preg_replace_callback('/\@([a-z\_\:0-9]+)\b/i', function($matches) use($data){
-        return $data->{$matches[1]};
-      }, $slug);
-      
+    foreach ($this->getSlugs($view, $view->dom->documentElement) as $template) {
+      $template->nodeValue = str_replace($template->matches, array_intersect_key((array)$data, $template->matches), $template->slug);
     }
   }
-  
-  
   
   public function getSlugs($view, $context)
   {
@@ -86,11 +61,14 @@ class Plat
     # and ends with the close placeholder
     $exp .= "substring(., string-length(.), 1) = ']']";
     
-    return $view->xpath->query($exp, $context);
-  }
-  
-  public function __clone()
-  {
-    $this->node = $this->node->cloneNode(true);
+    $nodes = $view->xpath->query($exp, $context);
+    
+    foreach ($nodes as $template) {
+      preg_match_all('/\@([a-z\_\:0-9]+)\b/i', substr($template->nodeValue, 1, -1), $matches);
+      $template->matches = array_combine($matches[1], $matches[0]);
+      ksort($template->matches);
+      $template->slug = substr($template->nodeValue, 1,-1);
+    }
+    return $nodes;
   }
 }
