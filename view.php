f0424570467bab0d690ef79b88ae40e54decfa58
Brendan <brendan.metzger@gmail.com>
2016-01-12 11:29:42 -0600

    apparently just some whitespace cleanup

--- view.php
+++ view.php
@@ -8,12 +8,12 @@ class View
 {
   public $dom, $xpath, $parser, $clone, $context = null;
   private $slugs = null;
-  
+
   private static $renderer = [
     'before'    => [],
     'after'     => [],
   ];
-	  
+
   public function __construct($document_element, $slugs = null)
   {
     $this->dom = new DOM\Document;
@@ -21,28 +21,28 @@ class View
     if (is_string($document_element)) {
       $this->dom->load(PATH.$document_element, LIBXML_COMPACT|LIBXML_NOBLANKS|LIBXML_NOXMLDECL|LIBXML_NOENT);
     } else if ($document_element instanceof \DOMNode) {
-      
+
       $this->dom->appendChild($this->dom->importNode($document_element, true));
     }
-        
+
     $this->xpath  = new \DomXpath($this->dom);
     $this->parser = new view\parser($this);
-    
+
     foreach ($this->parser->queryCommentNodes('insert') as $stub) {
       $path = trim(substr(trim($stub->nodeValue), 6));
       $element = $this->dom->importNode((new view($path))->dom->documentElement, true);
       $stub->parentNode->replaceChild($element, $stub);
     }
   }
-  
-  
+
+
   public function __set($key, $view)
   {
     $command = "replace {$key}";
     if (! $view instanceof view) {
       $view = new view($view);
     }
-    
+
     foreach ($this->parser->queryCommentNodes($command) as $stub) {
       $adjacency = trim(substr(trim($stub->nodeValue),strlen($command)));
 
@@ -59,33 +59,33 @@ class View
       }
     }
   }
-  
+
   static public function addRenderer($when, callable $callback)
   {
     self::$renderer[$when][] = $callback;
   }
-  
+
   public function getRenderer($key)
   {
     return self::$renderer[$key];
   }
-  
+
 	public function render($data = [])
-	{    
+	{
     foreach ($this->getRenderer('before') as $callback) {
       call_user_func($callback, $this);
     }
-    
+
     $this->parser->parse($data);
-    
+
     foreach ($this->getRenderer('after') as $callback) {
       call_user_func($callback, $this);
     }
-    
+
 
     return $this;
 	}
-  
+
   public function __toString()
   {
     return $this->dom->saveXML($this->context);
