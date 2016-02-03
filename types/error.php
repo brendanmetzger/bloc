22d0fc4c458541112bc16858732fbeb7f9f52cb8
Brendan <brendan.metzger@gmail.com>
2016-02-03 13:06:12 -0600

    warnings are captured

--- types/error.php
+++ types/error.php
@@ -10,7 +10,7 @@ set_error_handler(function($errno, $errstr, $errfile, $errline) {
   if ( E_RECOVERABLE_ERROR === $errno && strpos($errstr, 'must be an instance of') > 0) {
     \bloc\application::instance()->log($errfile . ':' . $errline);
     throw new Error($errstr, $errno);
-  } else if (E_NOTICE === $errno) {
+  } else if (E_NOTICE === $errno || E_WARNING === $errno) {
     \bloc\Application::instance()->log($errstr . "\nFile: " . $errfile . "\nLine: " . $errline);
     return true;
   }
