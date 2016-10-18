4b2c72778811bb08941c04ed7d50b1e44f26b180
Brendan <brendan.metzger@gmail.com>
2016-10-18 07:30:22 -0500

    adding a tostring method

--- types/dictionary.php
+++ types/dictionary.php
@@ -7,42 +7,46 @@ namespace bloc\types;
  */
 
 class Dictionary extends \ArrayIterator
-{  
+{
   use Map;
-  
+
   function __construct($data = [])
   {
     if (is_array($data) || is_object($data)) {
       parent::__construct($data);
     }
   }
-  
+
   public function __set($key, $value)
   {
     $this->offsetSet($key, $value);
   }
-  
+
   public function __get($key)
   {
     return $this->offsetGet($key);
   }
-  
 
-  
+
+
   public function offsetGet($offset)
   {
     if (!$this->offsetExists($offset)) {
-      throw new \RunTimeException("{$offset} is unavailable.", 404); 
+      throw new \RunTimeException("{$offset} is unavailable.", 404);
     }
-    
+
     $data = parent::offsetGet($offset);
     return is_array($data) ? new Dictionary($data) : $data;
   }
-  
+
   public function sort(callable $callback)
   {
     $this->uasort($callback);
     return $this;
   }
-  
+
+  public function __toString()
+  {
+    return '';
+  }
 }
